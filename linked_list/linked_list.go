fbcdda5618cd51dd3d43b324315829cfa599e035
Iago Ferreira <iago-ferreira@outlook.com>
2025-07-08 00:52:53 -0300

    feat: added linked_list functions

--- linked_list/linked_list.go
+++ linked_list/linked_list.go
@@ -1,7 +1,44 @@
-package linked_list
+package main
 
 import "fmt"
 
+type SinglyLinkedListNode struct {
+	data int32
+	next *SinglyLinkedListNode
+}
+
+type SinglyLinkedList struct {
+	head *SinglyLinkedListNode
+	tail *SinglyLinkedListNode
+}
+
+func (linkedList *SinglyLinkedList) Insert(data int32) {
+	node := &SinglyLinkedListNode{
+		next: nil,
+		data: data,
+	}
+	if linkedList.head == nil {
+		linkedList.head = node
+	} else {
+		linkedList.tail.next = node
+	}
+
+	linkedList.tail = node
+}
+
+func ReadNode(node *SinglyLinkedListNode) {
+	fmt.Println(node.data)
+	if node.next != nil {
+		ReadNode(node.next)
+	}
+}
+func (linkedList *SinglyLinkedList) Walk() {
+	ReadNode(linkedList.head)
+}
+
 func main() {
-	fmt.Println("Hello world")
+	linkedList := SinglyLinkedList{}
+	linkedList.Insert(10)
+	linkedList.Insert(20)
+	linkedList.Walk()
 }
