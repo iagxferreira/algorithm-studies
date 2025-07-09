b6d96d00803b7ca8534fa6fb0cf988a08ad96eb5
Iago Ferreira <iago-ferreira@outlook.com>
2025-07-09 18:31:31 -0300

    feat: added trees

--- main.go
+++ main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
-	"algorithm_studies/linked_list"
+	"algorithm_studies/tree"
 )
 
 func main() {
-	linkedList := linked_list.NewLinkedList()
-	linkedList.Insert(10)
-	linkedList.Insert(20)
-	linkedList.Insert(40)
-	linkedList.Insert(30)
-	linkedList.Unshift(20)
-	linkedList.Walk()
-	linkedList.Show()
+	tree := tree.NewTree()
+	tree.Insert(10)
+	tree.Insert(50)
+	tree.Insert(30)
+	tree.Insert(40)
+	tree.Insert(20)
+	tree.Insert(700)
+	tree.Insert(600)
+	tree.Insert(500)
+	tree.PrettyPrint()
 }
