92840583b3abc1a7ce349c94aae8b11347f1e290
Iago Ferreira <iago-ferreira@outlook.com>
2025-07-11 22:15:36 -0300

    feat: added trie dsa

--- main.go
+++ main.go
@@ -1,18 +1,9 @@
 package main
 
 import (
-	"algorithm_studies/tree"
+	"algorithm_studies/trie"
 )
 
 func main() {
-	tree := tree.NewTree()
-	tree.Insert(10)
-	tree.Insert(50)
-	tree.Insert(30)
-	tree.Insert(40)
-	tree.Insert(20)
-	tree.Insert(700)
-	tree.Insert(600)
-	tree.Insert(500)
-	tree.PrettyPrint()
+	trie.TestTrie()
 }
