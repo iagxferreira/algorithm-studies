f72d69a0894560e0ad51354da305336172052e6d
Iago Ferreira <iago-ferreira@outlook.com>
2026-01-13 21:21:41 -0300

    feat: changing folder structure

--- python/src/basics/24.py
+++ python/src/basics/24.py
@@ -1,9 +1,6 @@
 def main():
-    number = 1
-    while number <= 20:
-        number += 1
-        if number % 2 == 0:
-            print(number)
+    for i in range(10, 0, -1):
+        print(i)
 
 
 main()
